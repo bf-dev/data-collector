b923c6db0ff8318b341cbe01e45cc3dbbc1cf5b5
Insung Cho <bfdev@icloud.com>
2024-04-04 18:01:23 +0800

    feat: authencation and bump dependency version

--- main.ts
+++ main.ts
@@ -1,16 +1,20 @@
-import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
+import { Hono } from "https://deno.land/x/hono@v4.2.1/mod.ts";
+import { bearerAuth } from "https://deno.land/x/hono@v4.2.1/middleware/bearer-auth/index.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.42.0";
 
 const app = new Hono();
 
 const supabaseUri = Deno.env.get("SUPABASE_URI");
 const supabaseKey = Deno.env.get("SUPABASE_KEY");
+const authToken = Deno.env.get("AUTH_TOKEN");
 
-if (!supabaseUri || !supabaseKey) {
+if (!supabaseUri || !supabaseKey || !authToken) {
   throw new Error(
-    "SUPABASE_URI or SUPABASE_KEY environment variables are undefined.",
+    "SUPABASE_URI or SUPABASE_KEY or AUTH_TOKEN environment variables are undefined.",
   );
 }
+
+app.use('/*', bearerAuth({ token: authToken }))
 
 const supabase = createClient(supabaseUri, supabaseKey);
 
