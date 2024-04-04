3dc392df3bb5e3ade68d2126798f43c6d607a7dd
Insung Cho <bfdev@icloud.com>
2024-04-04 17:51:58 +0800

    feat: throw an error if environment variables did not set

--- main.ts
+++ main.ts
@@ -3,20 +3,26 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.42.0";
 
 const app = new Hono();
 
-const supabase = createClient(
-  Deno.env.get("SUPABASE_URI") ?? "",
-  Deno.env.get("SUPABASE_KEY") ?? ""
-);
+const supabaseUri = Deno.env.get("SUPABASE_URI");
+const supabaseKey = Deno.env.get("SUPABASE_KEY");
 
-app.post("/record/steps", async (context) =>{
+if (!supabaseUri || !supabaseKey) {
+  throw new Error(
+    "SUPABASE_URI or SUPABASE_KEY environment variables are undefined.",
+  );
+}
+
+const supabase = createClient(supabaseUri, supabaseKey);
+
+app.post("/record/steps", async (context) => {
   const { steps, startDate } = await context.req.json();
-  const hourlyDate = new Date(startDate)
-  hourlyDate.setMinutes(0,0,0)
+  const hourlyDate = new Date(startDate);
+  hourlyDate.setMinutes(0, 0, 0);
   await supabase.from("health_steps").upsert({
     date: hourlyDate,
-    steps
-  })
-  return context.text("OK")
-})
+    steps,
+  });
+  return context.text("OK");
+});
 
 Deno.serve(app.fetch);
